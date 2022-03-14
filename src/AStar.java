f8feeca15af676bee68b3848d7b22008ef4e9c74
tnechepurencko <321avisam5@gmail.com>
2022-03-14 12:59:01 +0300

    a*: some bugs fixed

--- src/AStar.java
+++ src/AStar.java
@@ -29,7 +29,7 @@ public class AStar extends Search {
         LinkedList<Position> shortestWay;
 
         while (!this.hp.exitLibrary) {
-            if (step == 27) {
+            if (step == 21) {
                 int breakpoint = 0;
             }
 
@@ -61,6 +61,11 @@ public class AStar extends Search {
                 }
             } else {
                 Position target = this.closestUnknown();
+
+                this.aStarCalculations[this.hp.position.x][this.hp.position.y][0] = 0;
+                this.aStarCalculations[this.hp.position.x][this.hp.position.y][1] =
+                        Math.abs(target.x - this.hp.position.x) + Math.abs(target.y - this.hp.position.y);
+
                 shortestWay = new LinkedList<>();
                 this.updateCalculations(this.hp.position, target);
                 this.roadMap[this.hp.position.x][this.hp.position.y] = 1;
@@ -86,6 +91,8 @@ public class AStar extends Search {
         this.roadMap[target.x][target.y] = 1;
         Position position = this.findPartOfShortestWay(target);
         while (!position.equals(this.hp.position)) {
+            this.printCalculations();
+
             shortestWay.add(position);
             this.roadMap[position.x][position.y] = 1;
             position = this.findPartOfShortestWay(position);
@@ -94,6 +101,7 @@ public class AStar extends Search {
         for (int i = shortestWay.size() - 1; i >= 0; i--) {
             this.hp.memory[this.hp.position.x][this.hp.position.y] = "x";
             this.hp.position = shortestWay.get(i);
+            this.getItem(field);
 
             this.checkAndPrint(field, stepNumber);
             stepNumber++;
@@ -101,31 +109,41 @@ public class AStar extends Search {
     }
 
     private void updateCalculations(Position current, Position target) {
-        int sum, heuristics;
+        int sum, heuristics, newSum, newHeuristics;
         for (int i = current.x - 1; i < current.x + 2; i++) {
             for (int j = current.y - 1; j < current.y + 2; j++) {
-                if (i > -1 && i < 9 && j > -1 && j < 9) {
-                    if (this.hp.memory[i][j].compareTo("n") != 0 && this.hp.memory[i][j].compareTo("f") != 0 &&
-                            this.hp.memory[i][j].compareTo("N") != 0 && this.hp.memory[i][j].compareTo("F") != 0) {
-                        sum = this.aStarCalculations[i][j][0] + this.aStarCalculations[i][j][1];
-                        heuristics = this.aStarCalculations[i][j][1];
-                         if (sum > Math.abs(target.x - current.x) + Math.abs(target.y - current.y) +
-                                Math.abs(i - current.x) + Math.abs(j - current.y)) {
-                            this.aStarCalculations[i][j][0] = Math.abs(i - current.x) +
-                                    Math.abs(j - current.y);
-                            this.aStarCalculations[i][j][1] = Math.abs(target.x - current.x) +
-                                    Math.abs(target.y - current.y);
-                        } else if (sum == Math.abs(target.x - current.x) + Math.abs(target.y - current.y) +
-                                Math.abs(i - current.x) + Math.abs(j - current.y) &&
-                                heuristics > Math.abs(i - current.x) + Math.abs(j - current.y)) {
-                            this.aStarCalculations[i][j][0] = Math.abs(i - current.x) +
-                                    Math.abs(j - current.y);
-                            this.aStarCalculations[i][j][1] = Math.abs(target.x - current.x) +
-                                    Math.abs(target.y - current.y);
-                        }
+                if (i > -1 && i < 9 && j > -1 && j < 9 &&
+                        this.hp.memory[i][j].compareTo("n") != 0 && this.hp.memory[i][j].compareTo("f") != 0 &&
+                        this.hp.memory[i][j].compareTo("N") != 0 && this.hp.memory[i][j].compareTo("F") != 0) {
+                    heuristics = this.aStarCalculations[i][j][1];
+                    sum = this.aStarCalculations[i][j][0] + this.aStarCalculations[i][j][1];
+                    newHeuristics = Math.abs(i - target.x) + Math.abs(j - target.y);
+                    newSum = newHeuristics + Math.max(Math.abs(i - current.x), Math.abs(j - current.y)) +
+                            this.aStarCalculations[current.x][current.y][0];
+
+                    if (sum > newSum) {
+                        this.aStarCalculations[i][j][0] = Math.max(Math.abs(i - current.x), Math.abs(j - current.y)) +
+                                this.aStarCalculations[current.x][current.y][0];
+                        this.aStarCalculations[i][j][1] = Math.abs(i - target.x) + Math.abs(j - target.y);
+                    } else if (sum == newSum && heuristics > newHeuristics) {
+                        this.aStarCalculations[i][j][1] = Math.abs(i - target.x) + Math.abs(j - target.y);
                     }
                 }
             }
+        }
+    }
+
+    private void getItem(Field field) {
+        if (this.hp.position.equals(field.book.position)) {
+            field.scheme[this.hp.position.x][this.hp.position.y] = "·";
+            this.hp.memory[this.hp.position.x][this.hp.position.y] = "·";
+            this.hp.hasBook = true;
+            System.out.println("BOOK FOUND");
+        } else if (this.hp.position.equals(field.cloak.position)) {
+            field.scheme[this.hp.position.x][this.hp.position.y] = "·";
+            this.hp.memory[this.hp.position.x][this.hp.position.y] = "·";
+            this.hp.hasCloak = true;
+            System.out.println("CLOAK FOUND");
         }
     }
 
@@ -140,11 +158,36 @@ public class AStar extends Search {
     }
 
     private Position findPartOfShortestWay(Position position) {
-        return this.findOptimalStep(position.x - 1, position.x + 2, position.y - 1, position.y + 2);
+        return this.findOptimalWay(position.x - 1, position.x + 2, position.y - 1, position.y + 2);
     }
 
     private Position doStep() {
         return this.findOptimalStep(0, 9, 0, 9);
+    }
+
+    private Position findOptimalWay(int i0, int iMax, int j0, int jMax) {
+        int minSum = 10000;
+        int minGain = 10000;
+        int x = -1, y = -1;
+
+        for (int i = i0; i < iMax; i++) {
+            for (int j = j0; j < jMax; j++) {
+                if (i > -1 && i < 9 && j > -1 && j < 9 && this.roadMap[i][j] == 0) {
+                    if (this.aStarCalculations[i][j][0] + this.aStarCalculations[i][j][1] < minSum) {
+                        minSum = this.aStarCalculations[i][j][0] + this.aStarCalculations[i][j][1];
+                        minGain = this.aStarCalculations[i][j][0];
+                        x = i;
+                        y = j;
+                    } else if (this.aStarCalculations[i][j][0] + this.aStarCalculations[i][j][1] == minSum &&
+                            minGain > this.aStarCalculations[i][j][0]) {
+                        minGain = this.aStarCalculations[i][j][0];
+                        x = i;
+                        y = j;
+                    }
+                }
+            }
+        }
+        return new Position(x, y);
     }
 
     private Position findOptimalStep(int i0, int iMax, int j0, int jMax) {
@@ -170,5 +213,19 @@ public class AStar extends Search {
             }
         }
         return new Position(x, y);
+    }
+
+    void printCalculations() {
+        for (int i = 8; i > -1; i--) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(this.aStarCalculations[i][j][0] + " " + this.aStarCalculations[i][j][1]);
+                if (this.aStarCalculations[i][j][0] != 10000) {
+                    System.out.print("        ");
+                }
+                System.out.print("; ");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
